83d110c2c52ae8d924d41c629456239f880db82b
voiceofriyad <Contactme.riyad@yahoo.com>
2026-06-06 01:24:47 +0600

    feat: Implementing time travel

--- src/App.jsx
+++ src/App.jsx
@@ -85,14 +85,22 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  // const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    // setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
-  function jumpTo(nextMove) {}
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
 
   const moves = history.map((squares, move) => {
     let description;
@@ -103,8 +111,13 @@ export default function Game() {
     }
 
     return (
-      <li>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+      <li key={move}>
+        <button
+          onClick={() => jumpTo(move)}
+          className="bg-gray-700 text-white mb-1 p-1 rounded cursor-pointer"
+        >
+          {description}
+        </button>
       </li>
     );
   });
